b016ac3b4759f4f99eaa2a1448ac0f45c6cf9986
Alf <alf.g.jr@gmail.com>
2020-08-24 22:38:05 -0700

    add top level public function mp4::read_mp4 for using the Mp4Reader with a file path. Added simple example.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::io::{BufReader};
+use std::fs::File;
+
 mod error;
 pub use error::Error;
 
@@ -17,3 +20,10 @@ pub use reader::Mp4Reader;
 
 mod writer;
 pub use writer::{Mp4Config, Mp4Writer};
+
+pub fn read_mp4(f: File) -> Result<Mp4Reader<BufReader<File>>> {
+    let size = f.metadata()?.len();
+    let reader = BufReader::new(f);
+    let mp4 = reader::Mp4Reader::read_header(reader, size)?;
+    Ok(mp4)
+}
