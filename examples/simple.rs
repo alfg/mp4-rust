be92897be6f21d3be1ce9a36f496a06ffc460801
Alf <alf.g.jr@gmail.com>
2020-08-26 20:45:18 -0700

    add criterion for benchmarking performance.

--- examples/simple.rs
+++ examples/simple.rs
@@ -6,7 +6,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        println!("Usage: mp4dump <filename>");
+        println!("Usage: simple <filename>");
         std::process::exit(1);
     }
 
